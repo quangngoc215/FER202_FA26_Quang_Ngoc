472145a286de5bfc90f70fad8ebbbebb145cf6c1
quangngoc215 <enderman22112005@gmail.com>
2026-09-14 08:15:27 +0700

    Initial commit: Slot1, Slot2, Slot3

---
+++ Slot3/my-first-app/src/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>My name is Quang Ngoc.</p>
+      <p>Email: quangngoc@example.com</p>q
+    </div>
+  );
+}
+
+export default About;
---
+++ Slot3/my-first-app/src/App.jsx
@@ -0,0 +1,12 @@
+import About from './About'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My First App</h1>
+      <About />
+    </div>
+  );
+}
+export default App
